61218d34f951561a6f31791003d221632e2dc2c8
Vladlena <107118802+Vladlena2@users.noreply.github.com>
2023-10-17 21:57:20 +0300

    Example of using variables of type charw

---
+++ Chapter3/TypeChar.java
@@ -0,0 +1,12 @@
+package Chapter3;
+
+public class TypeChar {
+        public static void main (String[] args) {
+            char chl, ch2;
+            chl = 88; // код для Х
+            ch2 = 'У';
+            System.out.print(" chl и ch2 : ");
+            System.out.println(chl + " " + ch2);
+            // Вывод - chl и ch2 : Х У
+        }
+}
