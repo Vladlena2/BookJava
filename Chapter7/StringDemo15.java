b2a95588b21978e763148bf5e63ef26584c21916
Vladlena <107118802+Vladlena2@users.noreply.github.com>
2023-10-31 21:23:48 +0300

    Add files via upload

---
+++ Chapter7/StringDemo15.java
@@ -0,0 +1,16 @@
+package Chapter7;
+// Продемонстрировать применение массивов объектов типа String
+public class StringDemo15 {
+    public static void main(String[] args) {
+        String str[] = {"один","два","три"};
+
+        for (int i = 0; i < str.length; i++)
+            System.out.println("str[" + i + "]: " + str[i]);
+    }
+}
+/*
+--------------------
+str[0]: один
+str[1]: два
+str[2]: три
+ */
